4e74c37329e57493a44c4ce62f55b1c2dfb750da
Martin Saulis <martin@saulis.com>
2022-04-07 01:41:27 +0300

    update main

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,33 @@
+/** @jsx h */
+/// <reference no-default-lib="true"/>
+/// <reference lib="dom" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.20/mod.ts";
+
+function App() {
+  return (
+    <html>
+    <head>
+      <title>Hello from JSX</title>
+    </head>
+    <body>
+    <h1>Hello world</h1>
+    </body>
+    </html>
+  );
+}
+
+function handler() {
+  const html = renderSSR(<App />);
+  return new Response(html, {
+    headers: {
+      "content-type": "text/html",
+    },
+  });
+}
 
 console.log("Listening on http://localhost:8000");
-serve((_req) => {
-  return new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
-  });
-});
+serve(handler);
