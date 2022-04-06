dee314e3696efa70502cce387435f0680d066c03
Martin Saulis <martin@saulis.com>
2022-04-07 01:33:50 +0300

    use deno

--- app/routes/hello[.]txt.tsx
+++
@@ -1,8 +0,0 @@
-export async function loader() {
-  return new Response(`Hello! This is a simple text file.`, {
-    status: 200,
-    headers: {
-      "Content-Type": "text/plain",
-    },
-  });
-}
